0acc6661e366d09692bc9325df893c8be84d249f
yjc229605957 <1198472052@qq.com>
2019-12-17 22:07:00 +0800

    学科模块 - api抽取 - 路由重定向 - 页面布局 - 初始数据获取 - 分页插件联动获取列表

---
+++ src/api/subject.js
@@ -0,0 +1,72 @@
+//导包
+import axios from 'axios'
+
+// 导入token工具函数
+import {
+  getToken
+} from '../utils/token';
+// 统一设置 axios的设置
+// axios.defaults 只能设置一个 axios对象
+// 如果项目中 可能用到多个 axios 支持创建一个
+// 后续的接口调用直接用  instance 即可
+
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_BASEURL,
+  //跨域携带cookie
+  withCredentials: true
+})
+//暴露出去 获取学科列表请求方法
+export function subjectList(params) {
+  return instance({
+    url: '/subject/list',
+    method: 'get',
+    headers: {
+      token: getToken()
+    },
+    params
+  })
+}
+//暴露出去 新增学科请求方法
+export function subjectAdd(data) {
+  return instance({
+    url: '/subject/add',
+    method: 'post',
+    headers: {
+      token: getToken()
+    },
+    data
+  })
+}
+//暴露出去 编辑学科请求方法
+export function subjectEdit(data) {
+  return instance({
+    url: '/subject/edit',
+    method: 'post',
+    headers: {
+      token: getToken()
+    },
+    data
+  })
+}
+//暴露出去 删除学科请求方法
+export function subjectRemove(data) {
+  return instance({
+    url: '/subject/remove',
+    method: 'post',
+    headers: {
+      token: getToken()
+    },
+    data
+  })
+}
+//暴露出去 学科状态请求方法
+export function subjectStatus(data) {
+  return instance({
+    url: '/subject/status',
+    method: 'post',
+    headers: {
+      token: getToken()
+    },
+    data
+  })
+}
--- src/views/index/children/subject.vue
+++ src/views/index/children/subject.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="comment">
+    <!-- 搜索栏 -->
     <div class="serachBar">
       <el-card class="box-card">
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
@@ -19,31 +20,33 @@
             </el-select>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">查询</el-button>
+            <el-button type="primary" @click="onSubject">查询</el-button>
           </el-form-item>
           <el-form-item>
             <el-button>清除</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="addSubmit" icon="el-icon-plus">新增学科</el-button>
+            <el-button type="primary" @click="addBut" icon="el-icon-plus">新增学科</el-button>
           </el-form-item>
         </el-form>
       </el-card>
     </div>
+    <!-- 学科列表分页 -->
     <div class="subjectList">
       <el-card class="box-card">
-        <el-table :data="tableData" style="width:100%" >
-          <el-table-column prop="NO" label="序号" width=70></el-table-column>
-          <el-table-column prop="number" label="学科编号" ></el-table-column>
-          <el-table-column prop="name" label="学科名称" ></el-table-column>
-          <el-table-column prop="forShort" label="简称" ></el-table-column>
-          <el-table-column prop="creator" label="创建者" ></el-table-column>
-          <el-table-column prop="date" label="创建日期" ></el-table-column>
-          <el-table-column prop="status" label="状态" ></el-table-column>
+        <el-table :data="tableData" style="width:100%">
+          <el-table-column prop="status" label="序号" width="70"></el-table-column>
+          <el-table-column prop="rid" label="学科编号"></el-table-column>
+          <el-table-column prop="name" label="学科名称"></el-table-column>
+          <el-table-column prop="short_name" label="简称"></el-table-column>
+          <el-table-column prop="username" label="创建者"></el-table-column>
+          <el-table-column prop="update_time" label="创建日期"></el-table-column>
+          <el-table-column prop="status" label="状态"></el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
               <el-button type="text" size="small">编辑</el-button>
               <el-button @click="handleClick(scope.row)" type="text" size="small">禁用</el-button>
+              <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -51,18 +54,50 @@
           background
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="currentPage4"
-          :page-sizes="[100, 200, 300, 400]"
-          :page-size="100"
+          :current-page="page"
+          :page-sizes="[7, 12, 30, 50]"
+          :page-size=pageSize
           layout="total, sizes, prev, pager, next, jumper"
-          :total="400"
+          :total="total"
         ></el-pagination>
       </el-card>
     </div>
+    <!-- 新增学科窗口 -->
+    <el-dialog title="新增学科" :visible.sync="dialogFormVisible" center>
+      <!-- 注册表单内容 -->
+      <el-form :model="addForm" :rules="addrules" ref="addForm" class="addForm">
+        <!-- 学科编号 -->
+        <el-form-item label="学科编号" :label-width="formLabelWidth" prop="rid">
+          <el-input v-model="addForm.rid" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 学科名称 -->
+        <el-form-item label="学科名称" :label-width="formLabelWidth" prop="name">
+          <el-input v-model="addForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 学科简称 -->
+        <el-form-item label="学科简称" :label-width="formLabelWidth">
+          <el-input v-model="addForm.short_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 学科简介 -->
+        <el-form-item label="学科简介" :label-width="formLabelWidth">
+          <el-input v-model="addForm.intro" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- 学科备注 -->
+        <el-form-item label="学科备注" :label-width="formLabelWidth">
+          <el-input v-model="addForm.remark" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部提交按钮 -->
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addSubject">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { subjectList, subjectAdd } from "../../../api/subject";
 export default {
   name: "subject",
   data() {
@@ -71,34 +106,83 @@ export default {
         user: "",
         region: ""
       },
-      tableData: [
-        {
-          NO: "1",
-          number: "QD001",
-          name: "前端与移动开发",
-          forShort: "前端",
-          creator: "刘经理",
-          date: "2019-11-21",
-          status: "启用",
-          operation: "编辑 禁用"
-        },
-        {
-          NO: "2",
-          number: "QD002",
-          name: "Java",
-          forShort: "后端",
-          creator: "刘经理",
-          date: "2019-11-21",
-          status: "启用",
-          operation: "编辑 禁用"
-        }
-      ]
+      tableData: [],
+      //当前页数
+      page: 1,
+      //总页数
+      pageCount: 10,
+      //每页条数
+      pageSize:7,
+      //总条数
+      total: 1,
+      // 新增学科窗口
+      dialogFormVisible: false,
+      // 新增学科数据
+      addForm: {
+        rid: "",
+        name: "",
+        short_name: "",
+        intro: "",
+        remark: ""
+      },
+      // 新增学科验证规则
+      addrules: {
+        rid: [{ required: true, message: "请输入学科编号", trigger: "change" }],
+        name: [{ required: true, message: "请输入学科名称", trigger: "change" }]
+      },
+      // 新增窗口文字标签宽度
+      formLabelWidth: "80px"
     };
   },
   methods: {
-    onSubmit() {
-      window.console.log("submit!");
+    //获取学科列表
+    SubList() {
+      subjectList({
+        page:this.page, //当前页数
+        limit:this.pageSize, //每页显示条数
+      }).then(res => {
+        window.console.log(res);
+        if (res.data.code === 200) {
+          this.tableData = res.data.data.items;
+          this.pageCount = res.data.data.pagination.page;
+          this.total = res.data.data.pagination.total;
+        }
+      });
+    },
+    //点击查询方法
+    onSubject() {},
+    // 点击新增按钮
+    addBut() {
+      this.dialogFormVisible = true;
+    },
+    //点击新增方法
+    addSubject() {
+      subjectAdd(this.addForm).then(res => {
+        window.console.log(res);
+        if (res.data.code === 200) {
+          this.dialogFormVisible = false;
+          this.$message.success("新增成功");
+          this.SubList();
+        }
+      });
+      window.console.log("111");
+    },
+    //分页插件 页码大小发生变动时触发的函数
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.SubList()
+      window.console.log(`每页 ${val} 条`);
+    },
+    //分页插件 当前页发生变动时触发的函数
+    handleCurrentChange(val) {
+      this.page = val
+      this.SubList()
+      window.console.log(`当前页: ${val}`);
     }
+  },
+  //已进入页面发送请求后去学科列表
+  created() {
+    this.SubList();
   }
 };
 </script>
@@ -127,8 +211,37 @@ export default {
   .box-card {
     height: auto;
   }
-  .el-pagination{
+  .el-pagination {
     text-align: center;
+  }
+}
+// 新增窗口样式
+.el-dialog__wrapper {
+  .el-dialog {
+    width: 600px;
+    height: 520px;
+
+    .el-dialog__header {
+      padding: 15px;
+      background: linear-gradient(225deg, #1493fa, #01c6fa);
+      .el-dialog__title {
+        color: #fff;
+      }
+    }
+    .el-dialog__body {
+      padding: 40px 25px 20px 20px;
+      .captcha {
+        height: 40px;
+        img {
+          height: 40px;
+          width: 100%;
+        }
+      }
+      .el-button--default {
+        width: 100%;
+        padding: 12px 0;
+      }
+    }
   }
 }
 </style>
